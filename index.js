48e7c11454d9a40de3dc8bf6cba429ed97c2d4fa
Dronexx3 <fabian.hilares@tecsup.edu.pe>
2024-04-06 16:59:30 -0500

    Mi tercer commit

--- index.js
+++ index.js
@@ -2,12 +2,51 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+
 const productos = [
     { id: 1, nombre: 'Producto 1', precio: 10 },
     { id: 2, nombre: 'Producto 2', precio: 20 },
     { id: 3, nombre: 'Producto 3', precio: 30 }
   ];
 
+const clientes = [
+    { id: 1, nombre: 'Juan Rojas', contacto: 'juan.rojas@gmail.com' },
+    { id: 2, nombre: 'Pedro Mamani', contacto: 'pedro.mamani@gmail.com' },
+    { id: 3, nombre: 'Mario Muñoz', contacto: 'mario.munoz@gmail.com' }
+];  
+
+app.get('/', (req, res) => res.send('Lab04 Soluciones en la nube'));
+app.get('/clientes', (req, res) => res.json(clientes));
+app.get('/clientes', (req, res) => res.json(clientes));
+
+app.post('/clientes', (req, res) => {
+    const nuevoCliente = req.body;
+    nuevoCliente.id = clientes.length + 1;
+    clientes.push(nuevoCliente);
+    res.status(201).send(nuevoCliente);
+});
+
+app.put('/clientes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const indice = clientes.findIndex(c => c.id === id);
+    if (indice !== -1) {
+        clientes[indice] = {...clientes[indice], ...req.body};
+        res.send(clientes[indice]);
+    } else {
+        res.status(404).send('Cliente inexistente');
+    }
+});
+
+app.delete('/clientes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const indice = clientes.findIndex(c => c.id === id);
+    if (indice !== -1) {
+        clientes.splice(indice, 1);
+        res.status(200).send(`Cliente con id ${id} eliminado`);
+    } else {
+        res.status(404).send('Cliente inexistente');
+    }
+});
 app.get('/productos', (req, res) => {
 res.json(productos);
 });
@@ -26,7 +65,7 @@ app.put('/productos/:id', (req, res) => {
       productos[indice] = {...productos[indice], ...req.body};
       res.send(productos[indice]);
   } else {
-      res.status(404).send('Producto no encontrado');
+      res.status(404).send('Producto inexistente');
   }
 });
 
@@ -37,9 +76,10 @@ app.delete('/productos/:id', (req, res) => {
       productos.splice(indice, 1);
       res.status(200).send(`Producto con id ${id} eliminado`);
   } else {
-      res.status(404).send('Producto no encontrado');
+      res.status(404).send('Producto inexistente');
   }
 });
+
 
 
 const port = process.env.PORT || 3000;
